5b6c1b04ffb1a53f3cda729d9ba21f983f1101fe
Arnova8G2 <easybuzz.eu@gmail.com>
2023-08-02 11:29:56 +0200

    Check if java is installed otherwise use downloaded and use java without install

--- Tools/GcamAPKTool/java.bat
+++ Tools/GcamAPKTool/java.bat
@@ -1,9 +1,42 @@
 @echo off
 SET ERROR = "error"		
 
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set SET_JAVA=java.exe
+%SET_JAVA% -version >NUL 2>&1
+if %ERRORLEVEL% equ 0 goto execute
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Download Java and use Java portable mod.
+
+goto download
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set SET_JAVA="%JAVA_HOME%bin\java.exe"
+
+if exist "%SET_JAVA%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Download Java and use Java portable mod.
+
+goto download
+
+:java_error
+echo %ERROR%
+PAUSE
+
+:download
 IF EXIST "%JAVA_SDK_TOOL%" (
-   GOTO build_with_java
+    SET SET_JAVA=%JAVA_SDK_TOOL%\bin\java.exe 
+    SET JAVA_HOME=%JAVA_SDK_TOOL%
+    GOTO execute
 )
+
 echo Download jdk8u252+9 java .....
 if not exist "%systemdrive%\Program Files (x86)" (
     SET "JAVA_ZIP=https://download.bell-sw.com/java/8u252+9/bellsoft-jdk8u252+9-windows-i586.zip"
@@ -11,7 +44,7 @@ if not exist "%systemdrive%\Program Files (x86)" (
     SET "JAVA_ZIP=https://download.bell-sw.com/java/8u252+9/bellsoft-jdk8u252+9-windows-amd64.zip"
 )
 
-echo Download java .....
+echo Download java...
 %CURL_TOOL% "%JAVA_ZIP%" -o java.zip >> "%LOG_FILE%"
 if errorlevel 1 (
 	 SET ERROR="java error download"
@@ -27,15 +60,9 @@ if errorlevel 1 (
 IF EXIST "java.zip" (
    DEL "java.zip" >> "%LOG_FILE%"
 )
-GOTO build_with_java
+GOTO download
 
-:java_error
-echo %ERROR%
-PAUSE
-
-:build_with_java
+:execute
 ECHO Run Java Runtime Environment
-SET SET_JAVA=%JAVA_SDK_TOOL%\bin\java.exe 
-SET JAVA_HOME=%JAVA_SDK_TOOL%
 SET path=%JAVA_HOME%/bin;%PATH%
 ECHO %path% >nul
